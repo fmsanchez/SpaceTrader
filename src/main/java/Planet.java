143cca0ea503e2957e2c0f17ae55aeedd4d6203e
Michael Lane <mlane@gatech.edu>
2014-09-23 22:15:11 -0400

    .java files go in src/main/java

---
+++ src/main/java/Planet.java
@@ -0,0 +1,17 @@
+public class Planet {
+
+	private Position pos;
+
+	public Planet(Position pos) {
+		this.pos = pos;
+	}
+
+	public Position getPos() {
+		return pos;
+	}
+
+	public void setPos(Position pos) {
+		this.pos = pos;
+	}
+
+}
