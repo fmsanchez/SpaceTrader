55660392cdee3f1481fd27553c6fdc3ae71414ec
Francisco <fmsanchez001@gmail.com>
2014-09-28 13:46:27 -0400

    Planet Universe Object modifications

--- src/Planet.java
+++ src/Planet.java
@@ -1,9 +1,15 @@
 public class Planet {
 
 	private Position pos;
+	private System system;
+	private int techLevel;
+	private String government;
 
-	public Planet(Position pos) {
+	public Planet(Position pos, System system, int techLevel, String government) {
 		this.pos = pos;
+		this.system = system;
+		this.techLevel = techLevel;
+		this.government = government;
 	}
 
 	public Position getPos() {
@@ -12,6 +18,10 @@ public class Planet {
 
 	public void setPos(Position pos) {
 		this.pos = pos;
+	}
+
+	public System getSystem() {
+		return system;
 	}
 
 }
